8c69d9de81683f4987645d5378927822cf83e54a
Mandar Vaze <mandarvaze@gmail.com>
2024-08-08 16:41:26 +0530

    Minor changes to the test after code restructuring

--- src/tests/test_domain.py
+++ src/tests/test_domain.py
@@ -1,6 +1,6 @@
-from domain_check import *
-import pytest
 import datetime 
+from lib.check_domain import get_domain_expiry_date
+
 
 def test_url():
     url = "https://www.greyneuronsconsulting.com"  # Replace with your URL
@@ -9,6 +9,6 @@ def test_url():
     if expiry_dates:
         for date in expiry_dates:
             print(f"Domain Expiry Date for {url}: {date}")
-            assert date ==  datetime.datetime(2025, 2, 10, 5, 17, 38)
+            assert date == datetime.datetime(2025, 2, 10, 5, 17, 38)
     else:
         print("Could not retrieve domain expiry date.")
